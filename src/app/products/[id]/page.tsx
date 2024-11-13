140e70b763f610c1bf053d1db159c1fa7cc5fe69
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-11-13 12:21:52 +0530

    Added params destructure from slug route

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,12 @@
-const Product = () => {
-  return <div>Product</div>;
+interface ProductParamsType {
+  params: {
+    id: string;
+  };
+}
+
+const Product = async ({ params }: ProductParamsType) => {
+  const { id } = await params;
+  return <div>Product: {id}</div>;
 };
 
 export default Product;
