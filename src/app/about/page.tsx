1fd0b75414e151fb29d93592c102faff7e60ea79
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-11-18 13:01:31 +0530

    useRouter() hook - Programaticaly handle route

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 const About = () => {
-  return <div>About</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>About</h1>
+      <button
+        className="bg-blue-400 text-white p-2 rounded-md"
+        onClick={() => router.push("/")}
+      >
+        Go Home
+      </button>
+    </div>
+  );
 };
 
 export default About;
