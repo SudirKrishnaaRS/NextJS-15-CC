89341993bdec4ac5d8e9b24eb497b129052aae91
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-11-18 18:29:04 +0530

    Added post - addUser() rest endpoint

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -8,3 +8,28 @@ export const users = [
 export async function GET() {
   return Response.json(users);
 }
+
+// addUser
+export async function POST(request: Request) {
+  // This is what we send from the Postman or ThunderClient body
+  const user = await request.json();
+
+  // Here we use the details sent from the postman and make a new user object
+  // which is to be added to the user array
+  const newUser = {
+    id: users.length + 1,
+    name: user.name,
+  };
+
+  // Adding to the user array
+  users.push(newUser);
+
+  // Returning a confirmation with status code 201 (meaning the user
+  // resource has been created)
+  return new Response(JSON.stringify(newUser), {
+    headers: {
+      "Content-type": "application/json",
+    },
+    status: 201,
+  });
+}
