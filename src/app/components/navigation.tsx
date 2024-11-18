6c93777ef61be8331d3f7c2600b3f74598347462
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-11-18 12:51:04 +0530

    Added active  style using usePathname() hook

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,15 +1,36 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navigation = () => {
+  const pathname = usePathname();
+
   return (
     <div>
-      <Link href={"/"} className="mr-5 text-blue-500">
+      <Link
+        href={"/"}
+        className={pathname === "/" ? "font-bold mr-4" : "mr-5 text-blue-500"}
+      >
         Home
       </Link>
-      <Link href={"/about"} className="mr-5 text-blue-500">
+
+      <Link
+        href={"/about"}
+        className={
+          pathname === "/about" ? "font-bold mr-4" : "mr-5 text-blue-500"
+        }
+      >
         About
       </Link>
-      <Link href={"/products/macbook"} className="mr-5 text-blue-500">
+
+      <Link
+        href={"/products/macbook"}
+        className={
+          pathname === "/products/macbook"
+            ? "font-bold mr-4"
+            : "mr-5 text-blue-500"
+        }
+      >
         Macbook
       </Link>
     </div>
