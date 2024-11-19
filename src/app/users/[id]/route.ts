a2da1787ff0951084d5de53d5c59330d053106b3
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-11-19 12:25:27 +0530

    PATCH - editUserbyId rest endpoint

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -15,3 +15,19 @@ export async function GET(_request: Request, { params }: ParamsType) {
   // Return the found user
   return Response.json(user);
 }
+
+// HINT: Own Implementation
+// PATCH - editUserbyId
+export async function PATCH(request: Request, { params }: ParamsType) {
+  const patchUser = await request.json();
+  // Get the `id` of the user from the URL
+  const { id } = await params;
+  // Find the user which matches the `id`
+  const user = users.find((user) => user.id === parseInt(id));
+  // If the user is found, update their name if provided
+  if (user && patchUser.name) {
+    user.name = patchUser.name;
+  }
+  // Return the found user
+  return Response.json(user);
+}
