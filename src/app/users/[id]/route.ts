58f389aa90b4476e73b8c3ee6893711f45252d96
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2024-11-19 13:02:06 +0530

    Added DELETE - deleteUserById rest endpoint

--- src/app/users/[id]/route.ts
+++ src/app/users/[id]/route.ts
@@ -31,3 +31,21 @@ export async function PATCH(request: Request, { params }: ParamsType) {
   // Return the found user
   return Response.json(user);
 }
+
+// HINT: Own Implementation
+// DELETE - deleteUserById
+export async function DELETE(_request: Request, { params }: ParamsType) {
+  // Get the `id` of the user from the URL
+  const { id } = await params;
+
+  // Find the index of the user with the specified `id`
+  const indexToDelete = users.findIndex((user) => user.id === parseInt(id));
+
+  // If a matching user is found, remove it
+  if (indexToDelete !== -1) {
+    //SYNTAX: splice(indexToDelete, deleteElementCount)
+    users.splice(indexToDelete, 1);
+  }
+  // Return a response confirming deletion
+  return Response.json({ message: "User deleted successfully" });
+}
